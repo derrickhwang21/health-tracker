771f4eaaaed96bdfba345ab85cbf0b04862fefe3
dhwang21 <hwang.derrick@gmail.com>
2019-01-08 12:00:50 -0800

    timer complete

--- app/src/main/java/com/hwang/health_tracker/MainActivity.java
+++ app/src/main/java/com/hwang/health_tracker/MainActivity.java
@@ -1,20 +1,29 @@
 package com.hwang.health_tracker;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int buttonClick = 0;
+    int seconds = 0;
+    boolean startRun;
+    long currentTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Timer();
     }
+
 
 
         public void onButtonClick(View v){
@@ -29,4 +38,36 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        public void onClickStart(View view){
+            startRun = true;
+        }
+
+        public void onClickStop(View view){
+            startRun = false;
+        }
+
+
+
+        public void Timer(){
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable(){
+            @Override
+            public void run(){
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                String time = String.format("%02d:%02d:%02d", hours, minutes, secs);
+
+                timeView.setText(time);
+
+                if(startRun){
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 100);
+            }
+        });
+        }
 }
