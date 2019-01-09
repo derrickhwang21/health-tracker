b3fca3d5417b89e3f4c2fee3679b3a431039e742
dhwang21 <hwang.derrick@gmail.com>
2019-01-09 12:29:20 -0800

    finished lab

--- app/src/main/java/com/hwang/health_tracker/MainActivity.java
+++ app/src/main/java/com/hwang/health_tracker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hwang.health_tracker;
 
 import android.os.Handler;
+import android.sax.TextElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,14 +14,21 @@ import android.widget.Toast;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     int buttonClick = 0;
+
     int seconds = 0;
     boolean startRun;
-    CarouselView carouselView;
 
-    int[] imageSlide = {R.drawable.man};
+    CarouselView carouselView;
+    CarouselView carouselLabel;
+
+    int[] imageSlide = {R.drawable.man, R.drawable.man2, R.drawable.man3};
+    int[] imageTitle = {R.drawable.label1, R.drawable.label2, R.drawable.label3};
+
 
 
     @Override
@@ -28,19 +37,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselLabel = (CarouselView) findViewById(R.id.carouselLabel);
         carouselView.setPageCount(imageSlide.length);
         carouselView.setImageListener(imageListener);
+        carouselLabel.setPageCount(imageTitle.length);
+        carouselLabel.setImageListener(imageListenerLabel);
 
 
         Timer();
     }
 
+
+
     ImageListener imageListener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
+
+            Log.d("position", "" + position +carouselLabel + imageTitle);
             imageView.setImageResource(imageSlide[position]);
         }
     };
+
+    ImageListener imageListenerLabel = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+
+            Log.d("position", "" + position +carouselLabel + imageTitle);
+            imageView.setImageResource(imageTitle[position]);
+        }
+    };
+
+
 
         public void onButtonClick(View v){
             buttonClick = buttonClick + 1;
