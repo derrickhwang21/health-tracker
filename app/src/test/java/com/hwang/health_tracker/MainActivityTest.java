044dbbbad284e0e61004b50747c6d3758d71359f
dhwang21 <hwang.derrick@gmail.com>
2019-01-09 21:10:02 -0800

    added readme and screenshots

---
+++ app/src/test/java/com/hwang/health_tracker/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.hwang.health_tracker;
+
+import android.view.View;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+
+    @Test
+    public void onButtonClickTest() { }
+
+    @Test
+    public void onClickStartTest() {
+    }
+
+    @Test
+    public void onClickStopTest() {
+    }
+
+    @Test
+    public void onClickResetTest() {
+
+    }
+
+    @Test
+    public void timerTest() {
+    }
+}
