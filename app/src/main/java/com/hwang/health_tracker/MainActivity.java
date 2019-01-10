9324bd15fdb076465b0bbbc2119a9d565b5a2a76
dhwang21 <hwang.derrick@gmail.com>
2019-01-10 13:08:52 -0800

    finished notification

--- app/src/main/java/com/hwang/health_tracker/MainActivity.java
+++ app/src/main/java/com/hwang/health_tracker/MainActivity.java
@@ -1,11 +1,22 @@
 package com.hwang.health_tracker;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -39,9 +50,41 @@ public class MainActivity extends AppCompatActivity {
         carouselLabel.setPageCount(imageTitle.length);
         carouselLabel.setImageListener(imageListenerLabel);
 
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+
+    public void onClickNotification(View v){
+            scheduleNotification(getNotification("Drink some water"), 5000);
+    }
 
 
 
+
+    private void scheduleNotification(Notification notification, int delay){
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content){
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Scheduled Notification");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.notification_icon);
+        return builder.build();
 
     }
 
