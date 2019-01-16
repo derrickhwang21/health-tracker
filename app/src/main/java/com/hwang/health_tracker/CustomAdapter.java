212bdb494b3b8c51c6af715f2a311d871b203b04
dhwang21 <hwang.derrick@gmail.com>
2019-01-16 11:18:08 -0800

    not working properly adding in self propogating data

--- app/src/main/java/com/hwang/health_tracker/CustomAdapter.java
+++ app/src/main/java/com/hwang/health_tracker/CustomAdapter.java
@@ -11,12 +11,19 @@ public class CustomAdapter extends BaseAdapter {
     Context context;
     String exerciseNameList[];
     int exerciseSetList[];
+    int exerciseRepsList[];
+    String exerciseDescriptionList[];
+    String exerciseTimeList[];
     LayoutInflater layInflater;
 
-    public CustomAdapter(Context applicationContext, String[] exerciseNameList, int[] exerciseSetList){
+    public CustomAdapter(Context applicationContext, String[] exerciseNameList, int[] exerciseSetList, int[] exerciseRepsList, String[] exerciseDescriptionList, String[] exerciseTimeList ){
         this.context = applicationContext;
         this.exerciseNameList = exerciseNameList;
         this.exerciseSetList = exerciseSetList;
+        this.exerciseRepsList = exerciseRepsList;
+        this.exerciseDescriptionList = exerciseDescriptionList;
+        this.exerciseTimeList = exerciseTimeList;
+
         layInflater = (LayoutInflater.from(applicationContext));
     }
 
@@ -40,8 +47,14 @@ public class CustomAdapter extends BaseAdapter {
         view = layInflater.inflate(R.layout.activity_exercise_list_view, null);
         TextView exerciseName = (TextView) view.findViewById(R.id.textViewName);
         TextView exerciseSets = (TextView) view.findViewById(R.id.textViewSets);
+        TextView exerciseReps = (TextView) view.findViewById(R.id.textViewReps);
+        TextView exerciseDescriptions = (TextView) view.findViewById(R.id.textViewDescription);
+        TextView exerciseTimes = (TextView) view.findViewById(R.id.textViewTime);
         exerciseName.setText(exerciseNameList[i]);
         exerciseSets.setText(exerciseSetList[i]);
+        exerciseReps.setText(exerciseRepsList[i]);
+        exerciseDescriptions.setText(exerciseDescriptionList[i]);
+        exerciseTimes.setText(exerciseTimeList[i]);
         return view;
     }
 }
--- app/src/main/java/com/hwang/health_tracker/ExcerciseActivityLog.java
+++ app/src/main/java/com/hwang/health_tracker/ExcerciseActivityLog.java
@@ -3,11 +3,8 @@ package com.hwang.health_tracker;
 import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -15,13 +12,15 @@ import java.util.List;
 public class ExcerciseActivityLog extends AppCompatActivity {
     AppDatabase database;
     ListView listView;
-    String exerciseNameList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
-    int setList[] = {1,2,3,4,5,6};
-    String[] nameArray1;
+    String exerciseNameList[] = {"Burpees", "Pushup"};
+    int setList[] = {1, 2};
+    int repList[] = {10, 12};
+    String descriptionList[] = {"All day", "You got this"};
+    String[] nameArray;
     int[] setArray;
-//    Integer[] setsArray;
-
-
+    int[] repArray;
+    String[] descriptionArray;
+    String[] timesArray;
 
 
     @Override
@@ -30,39 +29,38 @@ public class ExcerciseActivityLog extends AppCompatActivity {
         setContentView(R.layout.activity_exercise_log);
         database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "exercise").fallbackToDestructiveMigration().allowMainThreadQueries().build();
 
-
         if(database.excerciseDao().getAll().isEmpty()){
-
             for(int i = 0; i < exerciseNameList.length; i++) {
                 Date currentTime = Calendar.getInstance().getTime();
-                database.excerciseDao().add(new Exercise(exerciseNameList[i], setArray[i], 10, "burpees all day!", currentTime.toString()));
-
+                database.excerciseDao().add(new Exercise(exerciseNameList[i], setList[i], repList[i], descriptionList[i], currentTime.toString()));
             }
         }
-
-
-
-
+        // grabbing list data from room to array
         List<Exercise> exercises = database.excerciseDao().getAll();
-        nameArray1 = new String[exercises.size()];
+        nameArray = new String[exercises.size()];
         setArray = new int[exercises.size()];
-
-
-
+        repArray = new int[exercises.size()];
+        descriptionArray = new String[exercises.size()];
+        timesArray = new String[exercises.size()];
 
         for (int i = 0; i < exercises.size(); i++){
             String name = exercises.get(i).title;
             int sets = exercises.get(i).sets;
+            int reps = exercises.get(i).reps;
+            String descriptions = exercises.get(i).description;
+            String times = exercises.get(i).timestamp;
 
-            nameArray1[i] = name;
+            nameArray[i] = name;
             setArray[i] = sets;
-        }
+            repArray[i] = reps;
+            descriptionArray[i] = descriptions;
+            timesArray[i] = times;
 
-//        String[] nameArray = exercises.get(i).title.toArray(new String[exercises.size()]);
+        }
 
         listView = (ListView) findViewById(R.id.excerciseListView);
 
-        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), nameArray1, setArray);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), nameArray, setArray, repArray, descriptionArray, timesArray);
         listView.setAdapter(customAdapter);
 
     }
