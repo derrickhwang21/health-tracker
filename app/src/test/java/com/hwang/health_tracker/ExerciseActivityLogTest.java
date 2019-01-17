653e11e6f216e3c8312399b6f43e5b788f2a848a
dhwang21 <hwang.derrick@gmail.com>
2019-01-17 12:13:11 -0800

    initial commit for leftjoin

--- app/src/test/java/com/hwang/health_tracker/ExerciseActivityLogTest.java
+++ app/src/test/java/com/hwang/health_tracker/ExerciseActivityLogTest.java
@@ -6,20 +6,20 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
+//@RunWith(AndroidJUnit4.class)
 public class ExerciseActivityLogTest {
 
-    AppDatabase database;
-    Exercise exercise;
-
-    @Before
-    public void setUpDataBase(){
-        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "exercise").fallbackToDestructiveMigration().allowMainThreadQueries().build();
-    }
-
-
-    @Test
-    public void createExercise_AddNewExerciseObject_ReturnsTrue(){
-
-    }
+//    AppDatabase database;
+//    Exercise exercise;
+//
+//    @Before
+//    public void setUpDataBase(){
+//        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "exercise").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+//    }
+//
+//
+//    @Test
+//    public void createExercise_AddNewExerciseObject_ReturnsTrue(){
+//
+//    }
 }
